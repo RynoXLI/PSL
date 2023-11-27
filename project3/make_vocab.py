d4e83abd2f712bbc66888052fdcfdf65a7b3f4c2
Sean Enright <seanrenright@gmail.com>
2023-11-27 09:41:41 -0800

    use reduced vocabulary for prediction

--- project3/make_vocab.py
+++ project3/make_vocab.py
@@ -29,7 +29,7 @@ test_y = pd.read_csv(path_test_y, sep='\t', header=0, dtype=dtypes_dict)['sentim
 stop_words = [
     'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'their',
     'they', 'his', 'her', 'she', 'he', 'a', 'an', 'and', 'is', 'was', 'are', 'were', 'him',
-    'himself', 'has', 'have', 'it', 'its', 'the', 'us'
+    'himself', 'has', 'have', 'it', 'its', 'the', 'us', 'br'
 ]
 
 vectorizer = CountVectorizer(
@@ -56,8 +56,6 @@ class T_TestTransformer(BaseEstimator, TransformerMixin):
         mask = y.values.to_numpy() == 1
         pos_x = x[mask]
         neg_x = x[~mask]
-        print(f" Postive Size: {pos_x.shape[0]}")
-        print(f"Negative Size: {neg_x.shape[0]}")
         tstat = (pos_x.mean(axis=0) - neg_x.mean(axis=0)) / np.sqrt(
             pos_x.var(axis=0) / pos_x.shape[1] + neg_x.var(axis=0) / neg_x.shape[1]
         )
@@ -98,14 +96,8 @@ pd.DataFrame(vocab_lasso).to_csv('myvocab-lasso.txt', header=False, index=False)
 # TEST WITH REDUCED VOCAB
 # ============================
 
-vectorizer = CountVectorizer(
-    preprocessor=lambda x: x.lower(), # Convert to lowercase
-    stop_words=stop_words,            # Remove stop words
-    ngram_range=(1, 4),               # Use 1- to 4-grams
-    min_df=0.001,                     # Minimum term frequency
-    max_df=0.5,                       # Maximum document frequency
-    token_pattern=r"\b[\w+\|']+\b"    # Use word tokenizer
-)
+vectorizer = CountVectorizer(ngram_range=(1, 4))
+
 tfidf_transformer = TfidfTransformer()#use_idf=True)
 
 logistic_regression = LogisticRegressionCV(
@@ -116,18 +108,14 @@ logistic_regression = LogisticRegressionCV(
             scoring="roc_auc",
         )
 
-pipeline_predict = Pipeline([
-    ('vect', vectorizer),
-    ('tfidf', tfidf_transformer),
-    ('logreg', logistic_regression),
-])
+# Use reduced vocabulary to build model
+vectorizer.fit(vocab_lasso)
+x = vectorizer.transform(train_x)
+x = tfidf_transformer.fit_transform(x)
+model = logistic_regression.fit(x, train_y)
 
-# Spare matrix of shape (# train rows, vectorizer embedding size)
-start_time = time.time()
-pipeline_predict.fit(train_x, train_y)
-print(f"Prediction pipeline fitting: {(time.time() - start_time):.1f} s")
-
-start_time = time.time()
-preds = pipeline_predict.predict_proba(test_x)
+# Make predictions with test set
+x = vectorizer.transform(test_x)
+x = tfidf_transformer.fit_transform(x)
+preds = model.predict_proba(x)
 print(f"Test set AUC Score: {roc_auc_score(test_y, y_score=preds[:, 1]):.4f}")
-print(f"Prediction pipeline prediction: {(time.time() - start_time):.1f} s")
